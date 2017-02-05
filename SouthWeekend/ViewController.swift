e2729dbf08b7c270b5a53274941f2e2a6866ad33
daiyanggguang <you@example.com>
2017-02-05 21:38:35 +0800

    Initial Commit

---
+++ SouthWeekend/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SouthWeekend
+//
+//  Created by 随随意 on 17/2/5.
+//  Copyright © 2017年 suisuiyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
