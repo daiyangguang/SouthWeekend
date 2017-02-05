a0fcba3255d512e7b34061337e15b972c224a5c3
daiyanggguang <you@example.com>
2017-02-05 22:49:11 +0800

    add Router

---
+++ SouthWeekend/Network/Router/Router.swift
@@ -0,0 +1,16 @@
+//
+//  Router.swift
+//  SouthWeekend
+//
+//  Created by 随随意 on 17/2/5.
+//  Copyright © 2017年 suisuiyi. All rights reserved.
+//
+
+import Foundation
+
+struct Router {
+    let baseURL = "http://www.infzm.com/mobile"
+    let newHomePageUrl = "http://www.infzm.com/mobile/get_list_by_cat_ids?cat_id%5B%5D=5282&start=0&count=10&platform=ireader&device=Unknown%20iPad&version=5.2.3&system_version=10.2&hash=a2d1e67d2daa18d8781a7126c6982649&format=json"
+    
+
+}
--- SouthWeekend/UI/ViewController.swift
+++ SouthWeekend/UI/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        Thread.sleep(forTimeInterval: 0.25)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
