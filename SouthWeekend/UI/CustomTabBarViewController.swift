afd7864486729157e666dcc5d5c3da2eae7922ab
daiyanggguang <you@example.com>
2017-02-05 23:28:07 +0800

    config tabar

---
+++ SouthWeekend/UI/CustomTabBarViewController.swift
@@ -0,0 +1,34 @@
+//
+//  CustomTabBarViewController.swift
+//  SouthWeekend
+//
+//  Created by 随随意 on 17/2/5.
+//  Copyright © 2017年 suisuiyi. All rights reserved.
+//
+
+import UIKit
+
+class CustomTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBar.tintColor = UIColor.red
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
